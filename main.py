6eff922c60c01a182f183d075541406d96b53f4b
João Neves <joao.neves@enecad.com.br>
2020-05-26 14:44:03 -0300

    first test done: server status 200

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ import requests
 def get_temperature(lat, lng):
     key = 'e1ee55658d4a2b28c4841e373c3b3d87'
     url = 'https://api.darksky.net/forecast/{}/{},{}'.format(key, lat, lng)
-    reponse = requests.get(url)
-    data = reponse.json()
+    response = requests.get(url)
+    data = response.json()
     temperature = data.get('currently').get('temperature')
     if not temperature:
         return
     return int((temperature - 32) * 5.0 / 9.0)
+
+
+def get_response(lat, lng):
+    key = 'e1ee55658d4a2b28c4841e373c3b3d87'
+    url = 'https://api.darksky.net/forecast/{}/{},{}'.format(key, lat, lng)
+    response = requests.get(url)
+    return response
--- test_main.py
+++ test_main.py
@@ -1,5 +1,12 @@
-from main import get_temperature
+import main
+from unittest.mock import patch
 
 
-def test_get_temperature_by_lat_lng():
-    raise NotImplementedError
+@patch('main.requests.get')
+def test_server_is_online(get_mock):
+    get_mock.return_value.status_code = 200
+    lat = -14.235004
+    lng = -51.92528
+    response = main.get_response(lat, lng)
+    status = response.status_code
+    assert status == 200
