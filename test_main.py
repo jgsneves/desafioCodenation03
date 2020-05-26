d69a8a30945083a35ba8888c6ba76a321e901863
João Neves <joao.neves@enecad.com.br>
2020-05-26 15:53:39 -0300

    added: test server status code 500

--- test_main.py
+++ test_main.py
@@ -10,3 +10,13 @@ def test_server_is_online(get_mock):
     response = main.get_response(lat, lng)
     status = response.status_code
     assert status == 200
+
+
+@patch('main.requests.get')
+def test_server_is_offline(get_mock):
+    get_mock.return_value.status_code = 500
+    lat = -14.235004
+    lng = -51.92528
+    response = main.get_response(lat, lng)
+    status = response.status_code
+    assert status == 500
